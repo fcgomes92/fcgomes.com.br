11cf2ab61c247ba55d5113f3f0dc794f8c231eed
Fernando Gomes <fcgomes.92@gmail.com>
2017-12-22 11:55:02 -0200

    FIX: font protocol on index UPD: version

--- src/settings/settings.dev.js
+++ src/settings/settings.dev.js
@@ -1,4 +1,4 @@
-export const VERSION = 'DEV 2.1.4';
+export const VERSION = 'DEV 2.1.5';
 
 export const DEBUG = true;
 
--- src/settings/settings.prod.js
+++ src/settings/settings.prod.js
@@ -1,4 +1,4 @@
-export const VERSION = '2.1.4';
+export const VERSION = '2.1.5';
 
 export const DEBUG = false;
 
