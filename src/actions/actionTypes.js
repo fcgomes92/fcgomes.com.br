8791e34e582a4f43b51bbc3cb8d5116e3d92566c
Fernando Gomes <fcgomes.92@gmail.com>
2019-04-08 21:25:32 -0300

    firt commmit

--- src/actions/actionTypes.js
+++
@@ -1,5 +0,0 @@
-export const UPDATE_BLOG_POSTS = 'update_blog_posts',
-    UPDATE_BLOG_POSTS_ERROR = 'update_blog_posts_error',
-    UPDATE_BLOG_POSTS_LOADING = 'update_blog_posts_loading',
-    ADD_BLOG_POST = 'add_blog_post',
-    UPDATE_BLOG_POSTS_TOTAL_PAGES = 'update_blog_posts_total_pages';
